addf23a431e2bbaf21956e392d99b4299f0d30ad
PratikPatel13 <PratikPatel13@missionrnd.com>
2016-02-28 18:41:58 +0530

    Submit Commit

--- src/mergeSortedArrays.cpp
+++ src/mergeSortedArrays.cpp
@@ -1,6 +1,5 @@
 /*
-OVERVIEW: You are given 2 bank statements that are ordered by date - 
-Write a function that produces a single merged statement ordered by dates.
+OVERVIEW: You are given 2 bank statements that are ordered by date - Write a function that produces a single merged statement ordered by dates.
 E.g.: Input: A[3] = { { 10, "09-10-2003", "First" }, { 20, "19-10-2004", "Second" }, { 30, "03-03-2005", "Third" } };
 B[2] = { { 10, "09-10-2003", "First" }, { 220, "18-01-2010", "Sixth" } };
 Output: { { 10, "09-10-2003", "First" }, { 10, "09-10-2003", "First" }, { 20, "19-10-2004", "Second" },  30, "03-03-2005", "Third" }, { 220, "18-01-2010", "Sixth" } }
@@ -15,7 +14,7 @@ NOTES:
 */
 
 #include <iostream>
-
+int compare(char *s1, char *s2);
 struct transaction {
 	int amount;
 	char date[11];
@@ -23,5 +22,73 @@ struct transaction {
 };
 
 struct transaction * mergeSortedArrays(struct transaction *A, int ALen, struct transaction *B, int BLen) {
-	return NULL;
+	int i = 0, j = 0, k = 0;
+	struct transaction *mergeRes = NULL;
+	if (A == NULL || B == NULL)
+		return NULL;
+	mergeRes = (struct transaction *)malloc(sizeof(struct transaction)*(ALen + BLen));
+	while (k < ALen + BLen)
+	{
+		if (compare(A[i].date, B[j].date) == 1)
+		{
+			mergeRes[k] = B[j];
+			k++;
+			j++;
+		}
+		else
+		{
+			mergeRes[k] = A[i];
+			k++;
+			i++;
+		}
+		if (i == ALen)
+		{
+			while (j < BLen)
+			{
+				mergeRes[k] = B[j];
+				k++;
+				j++;
+			}
+		}
+		if (j == BLen)
+		{
+			while (i < ALen)
+			{
+				mergeRes[k] = A[i];
+				k++;
+				i++;
+			}
+		}
+	}
+	return mergeRes;
+}
+
+//return 0 if both are same, 1 if date1 is greater , -1 if date2 is greater
+int compare(char *date1, char *date2)
+{
+	int d1, d2, m1, m2, y1, y2;
+	sscanf(date1, "%d-%d-%d", &d1, &m1, &y1);
+	sscanf(date2, "%d-%d-%d", &d2, &m2, &y2);
+	if (y1 > y2)
+		return 1;
+	else if (y1 < y2)
+		return -1;
+	else
+	{
+		if (m1 > m2)
+		{
+			return 1;
+		}
+		else if (m1 < m2)
+			return -1;
+		else
+		{
+			if (d1 > d2)
+				return 1;
+			else if (d1 < d2)
+				return -1;
+			else
+				return 0;
+		}
+	}
 }
--- src/sortedArraysCommonElements.cpp
+++ src/sortedArraysCommonElements.cpp
@@ -1,6 +1,5 @@
 /*
-OVERVIEW: You are given 2 bank statements that are ordered by date. 
-Write a function that returns dates common to both statements
+OVERVIEW: You are given 2 bank statements that are ordered by date. Write a function that returns dates common to both statements
 (ie both statements have transactions in these dates).
 E.g.: Input: A[3] = { { 10, "09-10-2003", "First" }, { 20, "19-10-2004", "Second" }, { 30, "03-03-2005", "Third" } };
 B[3] = { { 10, "09-10-2003", "First" }, { 220, "18-01-2010", "Sixth" }, { 320, "27-08-2015", "Seventh" } };
@@ -10,13 +9,13 @@ INPUTS: Two bank statements (array of transactions).
 
 OUTPUT: Transactions having same dates.
 
-
 ERROR CASES: Return NULL for invalid inputs.
 
 NOTES:
 */
 
 #include <iostream>
+int isDateEqual(char *date1, char *date2);
 
 struct transaction {
 	int amount;
@@ -25,5 +24,23 @@ struct transaction {
 };
 
 struct transaction * sortedArraysCommonElements(struct transaction *A, int ALen, struct transaction *B, int BLen) {
-	return NULL;
+	int i, j, k = 0, flag = 0;
+	struct transaction *common = NULL;
+	if (A == NULL || B == NULL)
+		return NULL;
+	for (i = 0; i < ALen; i++)
+	{
+		for (j = 0; j < BLen; j++)
+		{
+			if (isDateEqual(A[i].date, B[j].date))
+			{
+				common = (struct transaction *)realloc(common,sizeof(struct transaction) * (k + 1));
+				common[k] = A[i];
+				k++;
+				break;
+			}
+		}
+
+	}
+	return common;
 }
