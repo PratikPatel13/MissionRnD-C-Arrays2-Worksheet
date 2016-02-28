25fa514f638968907a4a9d7fe664b6ff1139eaa2
PratikPatel13 <PratikPatel13@missionrnd.com>
2016-02-28 18:42:57 +0530

    Submit Commit

--- src/findSingleOccurenceNumber.cpp
+++ src/findSingleOccurenceNumber.cpp
@@ -11,10 +11,49 @@ OUTPUT: Element that occurs only once.
 ERROR CASES: Return -1 for invalid inputs.
 
 NOTES:
-There are better ways of solving the problem than a brute-force solution which is of O(n^2)
-complexity .
 */
-
+#include <iostream>
+void quicksort(int x[], int first, int last);
 int findSingleOccurenceNumber(int *A, int len) {
-	return -1;
+	int i;
+	if (A == NULL)
+		return -1;
+	quicksort(A, 0, len - 1);
+	for (i = 0; i < len; i = i + 3)
+	{
+		if (!(A[i] == A[i + 1] && A[i + 1] == A[i + 2]))
+		{
+			return A[i];
+		}
+	}
+
+}
+
+void quicksort(int x[], int first, int last){
+	int pivot, j, temp, i;
+
+	if (first<last){
+		pivot = first;
+		i = first;
+		j = last;
+
+		while (i<j){
+			while (x[i] <= x[pivot] && i<last)
+				i++;
+			while (x[j]>x[pivot])
+				j--;
+			if (i<j){
+				temp = x[i];
+				x[i] = x[j];
+				x[j] = temp;
+			}
+		}
+
+		temp = x[pivot];
+		x[pivot] = x[j];
+		x[j] = temp;
+		quicksort(x, first, j - 1);
+		quicksort(x, j + 1, last);
+
+	}
 }
